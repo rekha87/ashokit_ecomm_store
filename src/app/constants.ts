6ebe51b0a917f8a8ce4bc0c4d1463ce81891c22e
rekha87 <116466071+rekha87@users.noreply.github.com>
2024-12-10 16:57:13 +0000

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a9e43472984fb45be9de85e0f449a1f6-1484067401.eu-west-2.elb.amazonaws.com',
 };
